1ad0b0b3a538b1de771d334b6819dc9b1df4d748
ngwesinsandar2 <ngwesinsandar112@gmail.com>
2022-06-24 20:51:50 +0630

    ver1.2.0

--- script.js
+++ script.js
@@ -8,8 +8,8 @@ $(".portfolio").slick({
     {
       breakpoint: 1024,
       settings: {
-        slidesToShow: 3,
-        slidesToScroll: 3,
+        slidesToShow: 2,
+        slidesToScroll: 1,
         infinite: true,
         dots: true,
       },
@@ -17,8 +17,8 @@ $(".portfolio").slick({
     {
       breakpoint: 600,
       settings: {
-        slidesToShow: 2,
-        slidesToScroll: 2,
+        slidesToShow: 1,
+        slidesToScroll: 1,
       },
     },
     {
